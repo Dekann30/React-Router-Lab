66669ebdaba7be282607c2cb09bda10eaba0b38b
Deanna K <keysdeanna@gmail.com>
2022-05-23 00:46:00 -0400

    adds functioning routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Stocks from './pages/Stocks';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/stocks' element={<Stocks />} />
+      </Routes>
     </div>
   );
 }
