eb5f569f4b17e688a5906eecdcfa4746ff330e08
Deanna K <keysdeanna@gmail.com>
2022-05-24 00:10:57 -0400

    adds styling to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,15 +3,28 @@ import {Link} from 'react-router-dom'
 
 const Dashboard = function (props){
     return (
-        <div>
-            {stocks.map((stocks, idx) => {
+         <table>
+            <tr className='title'>
+                <th>Company Name(Symbol)</th>
+                <th>Price</th>
+                <th>Change</th>
+             </tr>
+             <tr>
+                 <th>{stocks.map((stocks, idx) => {
                 return(
                 <Link key={idx} to={`/stocks/${stocks.symbol}`}>
-                    <h2>{stocks.name}</h2>
-                </Link> 
-             )}
-            )}
-        </div>
+                    <h5>{stocks.name}({stocks.symbol})</h5>
+                </Link> )})}</th>
+                <th>{stocks.map((stocks, idx) => {
+                return(
+                    <h5>{stocks.lastPrice}</h5>)})}
+                </th>
+                <th>{stocks.map((stocks, idx) => {
+                return(
+                    <h5>{stocks.change.toFixed(2)}</h5>)})}</th>
+             </tr>
+            
+        </table>
     )
 }
 
